d1943ecf87e216373860d4246fcb6364682bb6ff
brookinsconsulting <info@brookinsconsulting.com>
2008-03-02 07:58:16 +0000

    Updated: Renamed extension directory
    git-svn-id: http://svn.projects.ez.no/ezxajax_classattributes@1649 8f3a7b1d-e673-48e6-97d5-eebfcdde74b5

---
+++ trunk/extension/ezxajax_classattributes/settings/design.ini.append.php
@@ -0,0 +1,9 @@
+<?php /*
+
+[ExtensionSettings]
+DesignExtensions[]=ezxajax_classattributes
+
+[JavaScriptSettings]
+JavaScriptList[]=ezxajax_classattributes.js
+
+*/ ?>
--- trunk/extension/xajax_classattributes/settings/design.ini.append.php
+++
@@ -1,9 +0,0 @@
-<?php /*
-
-[ExtensionSettings]
-DesignExtensions[]=xajax_classattributes
-
-[JavaScriptSettings]
-JavaScriptList[]=xajax_classattributes.js
-
-*/ ?>
